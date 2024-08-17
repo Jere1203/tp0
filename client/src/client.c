d6182a02799b361dfc57bf237828d59e9e3e8db3
Jere1203 <jeremias.bartolsic@hotmail.com>
2024-08-17 22:58:25 +0000

    rollback path

--- client/src/client.c
+++ client/src/client.c
@@ -68,7 +68,7 @@ t_log* iniciar_logger(void)
 t_config* iniciar_config(void)
 {
 	t_config* nuevo_config;
-	nuevo_config = config_create("/home/utnso/tp0/client/cliente.config");
+	nuevo_config = config_create("/home/utnso/Desktop/tp0/client/cliente.config");
 	if(nuevo_config == NULL)
 	{
 		printf("No se pudo acceder al config!\n");
